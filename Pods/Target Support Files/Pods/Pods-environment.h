6dbbcb8964929f40bedba4154fd2026e9fe6aa4c
Chris Morris <chris@morris.net>
2015-05-18 12:30:19 +0100

    Initial

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,27 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CoreBitcoin
+#define COCOAPODS_POD_AVAILABLE_CoreBitcoin
+#define COCOAPODS_VERSION_MAJOR_CoreBitcoin 0
+#define COCOAPODS_VERSION_MINOR_CoreBitcoin 6
+#define COCOAPODS_VERSION_PATCH_CoreBitcoin 7
+
+// ISO8601DateFormatter
+#define COCOAPODS_POD_AVAILABLE_ISO8601DateFormatter
+#define COCOAPODS_VERSION_MAJOR_ISO8601DateFormatter 0
+#define COCOAPODS_VERSION_MINOR_ISO8601DateFormatter 7
+#define COCOAPODS_VERSION_PATCH_ISO8601DateFormatter 0
+
+// OpenSSL-Universal
+#define COCOAPODS_POD_AVAILABLE_OpenSSL_Universal
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.0.1.j-2.
+
