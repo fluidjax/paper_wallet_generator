ef6c00a9d480251ace561320715a267748c10ec3
Chris Morris <chris@morris.net>
2015-05-21 14:39:52 +0100

    Force fail test

--- PaperWalletGenTests/PaperWalletGenTests.m
+++ PaperWalletGenTests/PaperWalletGenTests.m
@@ -27,7 +27,8 @@
 
 - (void)testExample {
     // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+//    XCTAssert(YES, @"Pass");
+    XCTAssert(NO, @"force fail");
 }
 
 - (void)testPerformanceExample {
